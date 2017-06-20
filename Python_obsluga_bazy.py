7f0c0a0c279f00b908e67580fd12033445046c89
Sebastian <sebastianzelazny@poczta.fm>
2017-06-20 22:49:25 +0200

    Dodana pełna obsługa zgłaszajacego

--- Python_obsluga_bazy.py
+++ Python_obsluga_bazy.py
@@ -23,20 +23,44 @@ class MenuDB:
             
         ###Repairer###############    
         if(self.cursor.rowcount == 1 and (self.rola == 'rep')):
+            self.ID_login_rep()
             self.repairer()       
         ####Reqester
         elif((self.cursor.rowcount == 1) and (self.rola == 'req')):
+            self.ID_login_req()
             self.requester()
         ####Admin    
         elif((self.cursor.rowcount == 1) and (self.rola == 'adm')):
             self.Admin()
         else:
             print('blad logowania')
-            
-            
+    def ID_login_rep(self):
+        self.ID_log=""
+        
+        self.sql12 ="SELECT * FROM repairers left join logins on repairers.ID_Login_rep=logins.ID_L where logins.login=%s"
+        self.cursor.execute(self.sql12,(self.login))  
+        self.results = self.cursor.fetchall()   					 
+        
+        for row in self.results:
+            self.ID_log = row[0]
+        
+        print("Wyciagniety ID LOG: "+str(self.ID_log)) 
+    def ID_login_req(self):
+        self.ID_log=""
+        
+        self.sql12 ="SELECT * FROM requester left join logins on requester.ID_Login_req=logins.ID_L where logins.login=%s"
+        self.cursor.execute(self.sql12,(self.login))  
+        self.results = self.cursor.fetchall()   					 
+        
+        for row in self.results:
+            self.ID_log = row[0]
+        
+        print("Wyciagniety ID LOG: "+str(self.ID_log))                             
+        
+        
     def requester(self):
         print('logowanie poprawne reqester')
-        self.i = input('Co chcesz zrobić: \n 1.-Dodaj zgłoszenie \n 2. Podejrzj zgłoszenie \n(Q)-wyjście \n Wybor: ')
+        self.i = input('Co chcesz zrobić: \n 1.-Dodaj zgłoszenie \n 2. Podejrzj zgłoszenie \n 3.Otwrzo ponownie zgłoszenie \n(Q)-wyjście \n Wybor: ')
         if(self.i == '1'):
             print("Dodaje zgłoszenie")
             self.list_of_repairers()
@@ -44,7 +68,13 @@ class MenuDB:
             self.logowanie()
         elif(self.i == '2'):
             print("Wyswietlam zgłoszenie")         
-            self.ShowReportsReq()         
+            self.ShowReportsReq()
+            self.logowanie()
+        elif(self.i=='3'):
+            print("Otwieram zgłoszenie")
+            self.ShowReportsReq()
+            self.OpenReport()
+            self.logowanie()
         elif((self.i == 'q') or (self.i == 'Q')):
             print("Koniec")
         else:
@@ -82,48 +112,6 @@ class MenuDB:
             print("Podałeś/aś nieprawidłową wartość wybierz 1,2,3,4,5,Q")
             self.repairer()
         
-        
-    def list_of_repairers(self):
-        self.Lista=[]
-        self.sql = "SELECT * FROM list_of_repairers"   						 
-        self.cursor.execute(self.sql)   						 
-        self.results = self.cursor.fetchall()   					 
-        #print ("%1s" % ("Logins") )
-        for row in self.results:
-            self.Login1 = row[0]
-            self.Lista.append(self.Login1)    
-        #print(self.logrep)             
-            
-    def AddReports(self):
-        self.list_of_repairers()
-        self.title = input("Podaj tytul Zgłoszenia: ")
-        self.descr = input("Podaj opis zgłoszenia: ")
-        self.logreq = input("Podaj swój login: ")
-        self.logrep = random.choice(self.Lista)
-        #print(self.logrep)
-        self.data=datetime.date.today()
-        #print(self.data)
-        self.prior = input("Podaj Priorytet: ") 
-        self.sql5 = "INSERT INTO reports (Title, description,Requester,Priority) VALUES (%s,%s,%s,%s)"
-        self.cursor.execute(self.sql5(self.title,self.descr,self.logreq,str(self.prior)))
-        self.conn.commit()
-        
-    def ShowReportsReq(self):   
-        
-        self.sql6 = "SELECT ID_Z,Title,description,priority_p,E_mail_rep,E_mail_req,Status_s,Data_R FROM view_request_how_requester WHERE Login=%s"   						 
-        self.cursor.execute(self.sql6,(self.login))
-        self.results = self.cursor.fetchall() 					 
-        print ("%5s%40s%40s%15s%25s%25s%15s%15s" % ("ID","Title","Description","Priorytet","E_mail_rep","E-mail_req","Status","Data"))
-        for row in self.results:
-            self.lp = row[0]
-            self.Title = row[1]
-            self.description = row[2]
-            self.priority = row[3]
-            self.repairerr = row[4]
-            self.requesterr = row[5]
-            self.Status = row[6]
-            self.Data_R = row[7]
-            print ("%5s%40s%40s%15s%25s%25s%15s%15s" % (self.lp, self.Title, self.description,self.priority,self.repairerr,self.requester,self.Status,self.Data_R,))
     
     def ShowReportsRep(self):
         self.sql7 = "SELECT ID_Z,Title,description,priority_p,E_mail_rep,E_mail_req,Status_s,Data_R FROM view_request_how_repairer WHERE Login=%s"
@@ -203,8 +191,73 @@ class MenuDB:
         
         
  
+#####################################      REQUESTER         ##########################################
  
  
+ 
+    def OpenReport(self):
+        self.ID = input("Podaj ID Zgłoszenia do Otwarcia: ")
+        self.status_z = input("Zmien Status na (2 - W realizacji )")
+        if(self.status_z=="2"):
+            self.sql11 = "UPDATE reports SET Status=%s WHERE (ID_Z=%s and requester=%s)"
+            self.cursor.execute(self.sql11,(self.status_z,self.ID,self.ID_log))
+            self.conn.commit()
+            print("Status zmieniono pomyślnie")
+        else:
+            print("Podano zły parametr dozwolony tylko 2 - W realizacji")
+            self.OpenReport        
+    
+    
+        
+    def list_of_repairers(self):
+        self.Lista=[]
+        self.sql = "SELECT * FROM list_of_repairers"   						 
+        self.cursor.execute(self.sql)   						 
+        self.results = self.cursor.fetchall()   					 
+        #print ("%1s" % ("Logins") )
+        for row in self.results:
+            self.Login1 = row[0]
+            self.Lista.append(self.Login1)    
+        #print(self.Lista)
+            
+    def AddReports(self):
+        self.title = input("Podaj tytul Zgłoszenia: ")
+        self.descr = input("Podaj opis zgłoszenia: ")
+        self.logrep = random.choice(self.Lista)
+        #print(self.logrep)
+        self.data=datetime.date.today()
+        #print(self.data)
+        self.prior = input("Podaj Priorytet: ") 
+        self.sql5 = "INSERT INTO reports (Title, description,Requester,Repairer,Data_R,priority) VALUES (%s,%s,%s,%s,%s,%s)"
+        self.cursor.execute(self.sql5,(self.title,self.descr,self.ID_log,self.logrep,self.data,self.prior))
+        self.conn.commit()
+        
+        
+        
+        
+    def ShowReportsReq(self):   
+        
+        self.sql6 = "SELECT ID_Z,Title,description,priority_p,E_mail_rep,E_mail_req,Status_s,Data_R FROM view_request_how_requester WHERE Login=%s"   						 
+        self.cursor.execute(self.sql6,(self.login))
+        self.results = self.cursor.fetchall() 					 
+        print ("%5s%40s%40s%15s%25s%25s%15s%15s" % ("ID","Title","Description","Priorytet","E_mail_rep","E-mail_req","Status","Data"))
+        for row in self.results:
+            self.lp = row[0]
+            self.Title = row[1]
+            self.description = row[2]
+            self.priority = row[3]
+            self.repairerr = row[4]
+            self.requesterr = row[5]
+            self.Status = row[6]
+            self.Data_R = row[7]
+            print ("%5s%40s%40s%15s%25s%25s%15s%15s" % (self.lp, self.Title, self.description,self.priority,self.repairerr,self.requesterr,self.Status,self.Data_R,))    
+     
+     
+     
+     
+     
+     
+     
        
     def wprowadzanie(self):   	 
         self.name = input('wprowadź imie: ')
